fa18cf5d2ee8a6ceb5f1c2fbb31b14863bbcacd6
elmurodvokhidov <elmurodvkh@gmail.com>
2024-03-27 09:26:33 +0500

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,15 @@ function App() {
     title: ""
   });
 
-  const id = useId();
+  const id = useId() + new Date().getMilliseconds();
 
   const [todos, dispatch] = useReducer(reducer, initialState);
 
   function reducer(state, action) {
     switch (action.type) {
-      case "ADD": return [...state, { ...action.payload, id }]
+      case "CREATE": return [...state, { ...action.payload, id }];
+      case "UPDATE": return state = state.map(todo => todo.id === action.payload.id ? { ...todo, title: action.payload.title } : todo);
+      case "DELETE": return state.filter(todo => todo.id !== action.payload);
 
       default: return state;
     }
@@ -23,16 +25,28 @@ function App() {
 
   const handleCreate = (e) => {
     e.preventDefault();
-    setInput({ title: "" });
     if (input.title !== "") {
-      dispatch({ type: "ADD", payload: input })
+      if (input.id) {
+        // ma'lumotni tahrirlash
+        dispatch({ type: "UPDATE", payload: input });
+      } else {
+        // yangi ma'lumot qo'shish
+        dispatch({ type: "CREATE", payload: input });
+      }
+      setInput({ title: "" });
     }
     else {
       alert("Please fill in the all blanks!");
     }
   };
 
-  console.log(todos);
+  const handleUpdate = (todo) => {
+    setInput(todo);
+  };
+
+  function deleteFunction(id) {
+    dispatch({ type: "DELETE", payload: id })
+  }
 
   return (
     <div className="flex items-center justify-center bg-gray-100 h-screen">
@@ -40,18 +54,40 @@ function App() {
         <h1 className="text-4xl uppercase text-center font-bold">To Do List</h1>
 
         <div className="flex flex-col justify-center items-start gap-4">
-          <input value={input.title} onChange={(e) => setInput({ ...input, title: e.target.value })} type="text" name="" id="" placeholder="Enter a task" className=" text-xl p-2 w-full rounded border border-gray-400" />
-          <button onClick={handleCreate} className="bg-gray-100 border border-black rounded capitalize px-4 py-1">add task</button>
+          <input
+            value={input.title}
+            onChange={(e) => setInput({ ...input, title: e.target.value })}
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter a task"
+            className=" text-xl p-2 w-full rounded border border-gray-400" />
+          <button
+            onClick={handleCreate}
+            className="bg-gray-100 border border-black rounded capitalize px-4 py-1">
+            {input.id ? "save" : "add task"}
+          </button>
         </div>
 
         <div className="max-h-80 overflow-y-auto flex flex-col gap-2 pt-2">
           {
             todos.map((todo, index) => (
-              <div key={index} className="w-full bg-gray-100 rounded-md flex items-center justify-between p-4">
+              <div
+                key={index}
+                className="w-full bg-gray-100 rounded-md flex items-center justify-between p-4">
                 <h1 className="text-xl">{todo.title}</h1>
                 <div className="flex gap-2">
-                  <button className="rounded-md capitalize py-2 px-4 bg-blue-500 text-white">edit</button>
-                  <button className="rounded-md capitalize py-2 px-4 bg-blue-500 text-white">delete</button>
+                  <button
+                    onClick={() => handleUpdate(todo)}
+                    className="rounded-md capitalize py-2 px-4 bg-blue-500 text-white">
+                    edit
+                  </button>
+
+                  <button
+                    onClick={() => deleteFunction(todo.id)}
+                    className="rounded-md capitalize py-2 px-4 bg-blue-500 text-white">
+                    delete
+                  </button>
                 </div>
               </div>
             ))
